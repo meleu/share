4d5d3e686fc61219fb4793c6c1ca111ef269e004
meleu <meleu@users.noreply.github.com>
2016-08-19 01:09:05 +0100

    Create task_screenshot_snippet.c

---
+++ task_screenshot_snippet.c
@@ -0,0 +1,17 @@
+// screenshots with the ROM name
+   if (settings->auto_screenshot_filename)
+   {
+      time_t cur_time;
+      time(&cur_time);
+      char format[PATH_MAX_LENGTH] = {0};
+
+      snprintf(format, sizeof(format),
+            "%s-%%Y%%m%%d-%%H%%M%%S.", path_basename(global_name_base));
+      strftime(shotname, sizeof(shotname), format, localtime(&cur_time));
+      strlcat(shotname, IMG_EXT, sizeof(shotname));
+   }
+   else
+   {
+      snprintf(shotname, sizeof(shotname),"%s.png", path_basename(global_name_base));
+   }
+   fill_pathname_join(filename, folder, shotname, sizeof(filename));
