fe3d89c92f4c08e3c7aaa1b40a1a2b7a2bdf7670
meleu <meleu@users.noreply.github.com>
2017-04-24 10:03:48 +0100

    Create iniGet.py

---
+++ iniGet.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+def iniGet(key, cfg_file):
+    ini_file = open(cfg_file, 'r')
+    pattern = r'[ |\t]*' + key + r'[ |\t]*=[ |\t]*'
+    value_m = r'"*([^"\|\r]*)"*'
+
+    for line in ini_file:
+        if re.match(pattern, line):
+            value = re.sub(pattern + value_m + '.*', r'\1', line)
+            print value
+
+iniGet(sys.argv[1], sys.argv[2])
