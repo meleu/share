d51d5894e290d0e0eed6604d37b3c058ac6b58ba
meleu <meleu@users.noreply.github.com>
2017-05-28 21:11:11 -0300

    Update es-tests.sh

--- es-tests.sh
+++ es-tests.sh
@@ -9,9 +9,21 @@
 #
 # meleu - May-2017
 
+
 # globals ####################################################################
+
+VERSION="alpha"
+
+# TESTERS: set NO_WARNING_FLAG to 1 if you don't want that warning message.
+NO_WARNING_FLAG=0
+
+SCRIPT_URL="https://raw.githubusercontent.com/meleu/share/master/es-tests.sh"
+SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd)"
+SCRIPT_NAME="$(basename "$0")"
 REPO_FILE="es-repos.txt"
-BACKTITLE="es-test.sh: Installing EmulationStation mods on your RetroPie"
+REPO_FILE_FULL="$SCRIPT_DIR/$REPO_FILE"
+BACKTITLE="$SCRIPT_NAME (version: $VERSION) - manage EmulationStation mods on your RetroPie"
 REPO_URL_TEMPLATE="https://github.com/"
 SRC_DIR="$HOME/src"
 RP_SETUP_DIR="$HOME/RetroPie-Setup"
@@ -20,8 +32,13 @@ RP_SUPPLEMENTARY_SRC_DIR="$RP_SETUP_DIR/scriptmodules/supplementary"
 RP_PACKAGES_SH="$RP_SETUP_DIR/retropie_packages.sh"
 RP_SUPPLEMENTARY_DIR="/opt/retropie/supplementary"
 
-# TESTERS: comment the line below if you don't want to see that warning message.
-dialog --backtitle "W A R N I N G !" --title " WARNING! " --yesno "\nThis script lets you install a non-official emulationstation mod in RetroPie. It is for those who want to test and help devs with feedback.\n\nBear in mind that many of those mods are still in development and you will need to know how to fix things if something break!\n\n\nDo you want to proceed?" 15 75 2>&1 > /dev/tty || exit
+[[ "$1" == "--no-warning" ]] && NO_WARNING_FLAG=1
+if [[ "$NO_WARNING_FLAG" == 0 ]]; then
+    dialog --backtitle "W A R N I N G !" --title " WARNING! " \
+    --yesno "\nThis script lets you install a non-official emulationstation mod in RetroPie. It is for those who want to test and help devs with feedback.\n\nBear in mind that many of those mods are still in development, and you need to know how to fix things if something break!\n\n\nDo you want to proceed?" \
+    15 75 2>&1 > /dev/tty \
+    || exit
+fi
 
 
 # dialog functions ##########################################################
@@ -62,7 +79,8 @@ function main_menu() {
             B "Build an ES repo/branch from the list" \
             E "Edit the ES repo/branch list" \
             C "Choose an installed ES branch to be the default" \
-            R "Remove an installed unofficial ES branch" \
+            R "Remove an installed ES branch" \
+            U "Update \"$0\" script" \
             2>&1 > /dev/tty)
 
         case "$choice" in
@@ -70,6 +88,7 @@ function main_menu() {
             E)  edit_repo_branch_menu ;;
             C)  set_default_es_menu ;;
             R)  remove_installed_es_menu ;;
+            U)  update_script ;;
             *)  break ;;
         esac
     done
@@ -77,7 +96,7 @@ function main_menu() {
 
 
 function build_es_branch_menu() {
-    if [[ ! -s "$REPO_FILE" ]]; then
+    if [[ ! -s "$REPO_FILE_FULL" ]]; then
         dialogMsg "\"$REPO_FILE\" is empty!\n\nAdd some ES repo/branch first."
         return 1
     fi
@@ -91,7 +110,7 @@ function build_es_branch_menu() {
         options=()
         while read -r repo branch; do
             options+=( $((i++)) "$repo ~ $branch" )
-        done < "$REPO_FILE"
+        done < "$REPO_FILE_FULL"
         choice=$(dialogMenu "List of available ES repository/branch to download, build and install (from \"$REPO_FILE\")." "${options[@]}") \
         || return 1
         repo=$(  echo "${options[2*choice-1]}" | tr -d ' ' | cut -d'~' -f1)
@@ -116,10 +135,10 @@ function es_download_build_install() {
     dialogYesNo "Are you sure you want to download, build and install ${developer}'s $branch ES branch?\n\n(Note: source files will be stored at \"$es_src_dir\")" \
     || return
 
-    dialogInfo "Downloading source files for ${developer}'s \"$branch\" ES branch..."
+    dialogInfo "Downloading source files for ${developer}'s $branch ES branch..."
     gitPullOrClone "$es_src_dir" "$repo" "$branch"
 
-    dialogInfo "Building ${developer}'s \"$branch\" ES branch..."
+    dialogInfo "Building ${developer}'s $branch ES branch..."
     if ! build_es; then
         echo "====== W A R N I N G !!! ======"
         echo "= SOMETHING WRONG HAPPENED!!! ="
@@ -129,7 +148,7 @@ function es_download_build_install() {
         return 1
     fi
 
-    dialogInfo "Installing ${developer}'s \"$branch\" ES branch in \"$es_install_dir\"."
+    dialogInfo "Installing ${developer}'s $branch ES branch in \"$es_install_dir\"."
     if ! install_es; then
         echo "====== W A R N I N G !!! ======"
         echo "= SOMETHING WRONG HAPPENED!!! ="
@@ -139,20 +158,19 @@ function es_download_build_install() {
         return 1
     fi
 
-    dialogMsg "SUCCESS!\n\nThe ${developer}'s EmulationStation \"$branch\" branch was successfully installed!\n\nThis version is now the default emulationstation on your system.\n\nYou can choose which ES version will be the default one using the \"C\" option at Main Menu."
+    dialogMsg "SUCCESS!\n\nThe ${developer}'s EmulationStation $branch branch was successfully installed!\n\nThis ES version is now the default emulationstation on your system.\n\nYou can choose which ES version will be the default one using the \"C\" option at Main Menu."
 }
 
 
 function build_es() {
     local ret=0
-    local make_clean_flag=0
 
     cd "$es_src_dir"
     rpSwap on 512
     cmake . -DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2/ || ret=1
     # following RetroPie user Hex's suggestion [https://retropie.org.uk/forum/post/81034]
-    dialogYesNo "We are ready to compile ${developer}'s \"$branch\" ES branch.\n\nWould you like to omit the 'make clean' before 'make'?\n(compilation will be much faster)" \
-    || make clean
+    # do not "make clean" for a faster compilation
+    #make clean
     make || ret=1
     rpSwap off
     cd -
@@ -214,14 +232,14 @@ function edit_repo_branch_menu() {
 
     while true; do
         options=(A "Add an ES repo/branch to the list")
-        [[ -s "$REPO_FILE" ]] && options+=(D "DELETE ALL REPO/BRANCHES IN THE LIST")
+        [[ -s "$REPO_FILE_FULL" ]] && options+=(D "DELETE ALL REPO/BRANCHES IN THE LIST")
 
         while read -r line; do
             line_number=$(echo "$line" | cut -d: -f1)
             developer=$(echo "$line" | sed "s#.*https://github.com/\([^/]*\)/.*#\1#")
             branch=$(echo "$line" | sed "s,[^ ]* *,,")
             options+=( "$line_number" "Delete ${developer}'s \"$branch\" branch" )
-        done < <(nl -s: -w1 "$REPO_FILE")
+        done < <(nl -s: -w1 "$REPO_FILE_FULL")
 
         choice=$(dialogMenu "Edit the ES repository/branch list \"$REPO_FILE\"." "${options[@]}") \
         || return 1
@@ -229,15 +247,15 @@ function edit_repo_branch_menu() {
         case "$choice" in
             A)  add_repo_branch
                 ;;
-            D)  dialogYesNo "Are you sure you want to delete every single entry in \"$REPO_FILE\"?" \
+            D)  dialogYesNo "Are you sure you want to delete every single entry in \"$REPO_FILE_FULL\"?" \
                 || continue
-                echo -n > "$REPO_FILE"
+                echo -n > "$REPO_FILE_FULL"
                 ;;
-            *)  repo=$(  sed -n ${choice}p "$REPO_FILE" | cut -d' ' -f1)
-                branch=$(sed -n ${choice}p "$REPO_FILE" | cut -d' ' -f2)
-                dialogYesNo "Are you sure you want to delete the following line from \"$REPO_FILE\"?\n\n$repo $branch" \
+            *)  repo=$(  sed -n ${choice}p "$REPO_FILE_FULL" | cut -d' ' -f1)
+                branch=$(sed -n ${choice}p "$REPO_FILE_FULL" | cut -d' ' -f2)
+                dialogYesNo "Are you sure you want to delete the following line from \"$REPO_FILE_FULL\"?\n\n$repo $branch" \
                 || continue
-                sed -i ${choice}d "$REPO_FILE"
+                sed -i ${choice}d "$REPO_FILE_FULL"
                 remove_source_files "$SRC_DIR/$(friendly_repo_branch_name)"
                 ;;
         esac
@@ -249,7 +267,7 @@ function add_repo_branch() {
     local form=()
     local new_repo="$REPO_URL_TEMPLATE"
     local new_branch
-
+    
     while true; do
         form=( $(dialog --form "Enter the ES repository URL and the branch name."  17 75 5 \
             "URL    :" 1 1 "$new_repo"   1 10 80 0 \
@@ -261,7 +279,7 @@ function add_repo_branch() {
 
         dialogInfo "Adding \"$new_branch\" to the the list.\nPlease wait..."
         validate_repo_branch "$new_repo" "$new_branch" || continue
-        echo "$new_repo $new_branch" >> "$REPO_FILE"
+        echo "$new_repo $new_branch" >> "$REPO_FILE_FULL"
         return 0
     done
 }
@@ -271,7 +289,7 @@ function validate_repo_branch() {
     local repo="$1"
     local branch="$2"
 
-    if grep -qi "$repo *$branch" "$REPO_FILE" ; then
+    if grep -qi "$repo *$branch" "$REPO_FILE_FULL" ; then
         dialogMsg "The \"$branch\" branch from \"$repo\" is already in the list!\n\nNothing will be changed."
         return 1
     fi
@@ -301,7 +319,7 @@ function set_default_es_menu() {
             options+=( $((i++)) "$es_branch" )
         done
 
-        choice=$(dialogMenu "List of installed ES branches on \"$RP_SUPPLEMENTARY_DIR\".\n\nWhich one do you want to set as the default emulationstation?" "${options[@]}") \
+        choice=$(dialogMenu "List of installed ES branches on \"$RP_SUPPLEMENTARY_DIR\".\n\nWhich one you want to set as the default emulationstation?" "${options[@]}") \
         || return 1
         es_branch="${options[2*choice-1]}"
 
@@ -320,7 +338,7 @@ function set_default_es_menu() {
 
 
 function get_installed_branches() {
-    local installed_branches=( $(find "$RP_SUPPLEMENTARY_DIR" -type f -name emulationstation.sh ! -path '*/configscripts/*') )
+    local installed_branches=( $(find "$RP_SUPPLEMENTARY_DIR" -type f -name emulationstation.sh) )
     local b
     for b in "${installed_branches[@]}"; do
         basename "$(dirname "$b")"
@@ -337,17 +355,10 @@ function remove_installed_es_menu() {
     while true; do
         i=1
         for es_branch in $(get_installed_branches); do
-            [[ "$es_branch" == "emulationstation" || "$es_branch" == "emulationstation-kids" ]] \
-            && continue
             options+=( $((i++)) "$es_branch" )
         done
 
-        if [[ -z "${options[@]}" ]]; then
-            dialogMsg "There's no unofficial ES branches installed on \"$RP_SUPPLEMENTARY_DIR\"!"
-            return 1
-        fi
-
-        choice=$(dialogMenu "List of installed unofficial ES branches on \"$RP_SUPPLEMENTARY_DIR\".\n\nWhich one would you like to uninstall?" "${options[@]}") \
+        choice=$(dialogMenu "List of installed ES branches on \"$RP_SUPPLEMENTARY_DIR\".\n\nWhich one you want to uninstall?" "${options[@]}") \
         || return 1
         es_branch="${options[2*choice-1]}"
 
@@ -358,12 +369,15 @@ function remove_installed_es_menu() {
 
         if rp_scriptmodule_action remove "$es_branch"; then
             if sudo "$RP_PACKAGES_SH" emulationstation configure; then
-                dialogMsg "SUCCESS!\n\nThe \"$es_branch\" ES branch was uninstalled!\n\nThe official RetroPie ES is now the default emulationstation."
+                dialogMsg "SUCCESS!\n\nThe \"$es_branch\" ES branch was uninstalled!\nThe official RetroPie ES is now the default emulationstation."
+                return 0
             else
                 dialogMsg "WARNING!\n\nThe \"$es_branch\" ES branch was uninstalled.\nBut we failed to set the official RetroPie ES as the default emulationstation. You need to sort it."
+                return 2
             fi
         else
             dialogMsg "FAIL!!\n\nFailed to uninstall the \"$es_branch\" ES branch."
+            return 1
         fi
     done
 }
@@ -380,8 +394,31 @@ function remove_source_files() {
     if [[ -d "$es_src_dir" ]]; then
         dialogYesNo "Do you want to remove source files from \"$es_src_dir\" too?" \
         && rm -rf "$es_src_dir" \
-        && dialogMsg "Source files from \"$es_src_dir\" has been removed."
+        && dialogMsg "Source files from \"$es_src_dir\" has been removed." \
+        || dialogMsg "Failed to remove \"$es_src_dir\"."
     fi
+}
+
+
+function update_script() {
+    local err_flag=0
+    local err_msg
+
+    dialogYesNo "Are you sure you want to download the latest version of \"$SCRIPT_NAME\" script?" \
+    || return 1
+
+    err_msg=$(wget "$SCRIPT_URL" -O "/tmp/$SCRIPT_NAME" 2>&1) \
+    && err_msg=$(cp "/tmp/$SCRIPT_NAME" "$SCRIPT_DIR/$SCRIPT_NAME" 2>&1) \
+    || err_flag=1
+
+    if [[ $err_flag -ne 0 ]]; then
+        err_msg=$(echo "$err_msg" | tail -1)
+        dialogMsg "Failed to update \"$SCRIPT_NAME\".\n\nError message:\n$err_msg"
+        return 1
+    fi
+
+    [[ -x "$SCRIPT_DIR/$SCRIPT_NAME" ]] && exec "$SCRIPT_DIR/$SCRIPT_NAME" --no-warning
+    return 1
 }
 
 # START HERE #################################################################
