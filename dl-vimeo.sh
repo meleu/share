e35a9c6bdd3104c0f9c12e42638288f64a0d1250
meleu <meleu@users.noreply.github.com>
2019-08-08 14:26:13 -0300

    Update dl-vimeo.sh

--- dl-vimeo.sh
+++ dl-vimeo.sh
@@ -9,28 +9,26 @@
 
 
 INPUT_FILE="$1"
-OUTPUT_DIR=''
+OUTPUT_DIR="$2"
 
 [[ -f "$INPUT_FILE" ]] || exit 1
+[[ -d "$OUTPUT_DIR" ]] || exit 1
 
 readonly VIMEO_REGEX='^https:\/\/player\.vimeo\.com\/video\/[0-9]+'
 readonly URL_REGEX='^http.:\/\/'
 
 while IFS='' read -r line; do
-    temp="$(echo "$line" | grep -Eo "$VIMEO_REGEX")"
-
     if [[ -n "$vimeo_url" ]]; then
-        if [[ $temp =~ $URL_REGEX ]]; then 
-            youtube-dl -o "$OUTPUT_DIR/%(title)s-%(id)s.%(ext)s" -c "$vimeo_url" --referrer "$temp"
+        if [[ $line =~ $URL_REGEX ]]; then 
+            echo "Baixando \"$vimeo_url\"..."
+            youtube-dl -o "${OUTPUT_DIR}%(title)s-%(id)s.%(ext)s" -c "$vimeo_url" --referer "$line"
         fi
     fi
 
-    if [[ $temp =~ $VIMEO_REGEX ]]; then
-        vimeo_url="$temp"
+    if [[ $line =~ $VIMEO_REGEX ]]; then
+        vimeo_url="$line"
         continue
     fi
 
     vimeo_url=''
 done < "$INPUT_FILE"
-
-
